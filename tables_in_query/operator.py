70600c26f341cb32dbb1add1194aaadd040ffa3a
Aabhash <aabhashd@outlook.com>
2018-05-11 11:39:05 +0545

    Divide code into operator and helper functions

---
+++ tables_in_query/operator.py
@@ -0,0 +1,28 @@
+from helper import *
+
+def main(): 
+
+    print("Reading file paths")
+
+    table_file = "tables.txt"
+    query_file = 'query.txt'
+    output_file = 'output.txt'
+
+    print("Reading list of tables from file {0}".format(table_file))
+    table_list = read_tables(table_file)
+    table_dict = convert_to_dictionary(table_list)
+    print("Table List Obtained")
+
+    print("Reading query from file {0}".format(query_file))
+    query = read_query(query_file)
+    cleaned_query = remove_whitespace(query)
+    words_in_query = wordify_query(cleaned_query)
+
+    print("Getting tables from query")
+    tables_in_query = get_tables_in_query(table_dict, words_in_query)
+
+    print("Writing to file")
+    write_to_file(output_file, tables_in_query)
+        
+if __name__ == "__main__":
+    main()
