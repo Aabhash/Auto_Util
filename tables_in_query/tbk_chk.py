8ddc725cf04c6052c72c85896e4d35b2d88b71ed
Aabhash <aabhashd@outlook.com>
2018-05-11 11:12:53 +0545

    Divide into functions

--- tables_in_query/tbk_chk.py
+++ tables_in_query/tbk_chk.py
@@ -1,9 +1,25 @@
 import re
 
+def read_tables(table_file):
+    l = []
+    with open(table_file, 'r') as f:
+         l = (f.read().split('\n'))
+    return l
+
+def read_query(query_file):
+    with open(query_file, 'r') as r:
+        query = r.read()
+    return query
+
+def remove_whitespace(query):
+    return " ".join(query.split())
+
+def wordify_query(q):
+    return re.split('(\W+)', query)
+
 table_dict = {}
 table_file = "file.txt"
-with open(table_file, 'r') as f:
-    list_of_tables = f.read().split('\n')
+table_list = read_tables(table_file)
 
 for i in range(len(list_of_tables)):
     table_dict[list_of_tables[i]] = i
@@ -11,12 +27,10 @@ print(table_dict)
 print("*********************TABLE LIST OBTAINED****************************\n\n")
 
 query_file = 'query.txt'
-query = ''
-with open(query_file, 'r') as r:
-    query = r.read()
-" ".join(query.split())
+query = read_query(query_file)
+cleaned_query = remove_whitespace(query)
+query_word_list = wordify_query(cleaned_query)
 
-query_word_list = re.split('(\W+)', query)
 tables_in_query = set()
 for word in query_word_list:
     if (word in table_dict):
