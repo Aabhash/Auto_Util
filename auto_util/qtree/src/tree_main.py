f18bb27d76705584de337f23ec22340cf778c484
Aabhash Dhakal <aabhashd@outlook.com>
2018-05-30 23:38:19 +0545

    Finesse out preliminary logic for query tree

--- auto_util/qtree/src/tree_main.py
+++ auto_util/qtree/src/tree_main.py
@@ -1,13 +1,23 @@
 import re
-print()
-query = 'SELECT * FROM TAB WHERE T = 2'
-query_block1 = re.search('SELECT *.* FROM', query)
-query_block2 = re.search('FROM *.* WHERE', query)
-query_block3 = re.search('WHERE *.*', query)
-print(query_block1.group(0))
-print(query_block2.group(0))
-print(query_block3.group(0))
 
-#Shotest distance between any two keywords
-#OR List of keywords
-#Get the meat between keywords
+print()
+query = 'SELECT RLS.OK FROM TAB WHERE T = 2 ORDER BY OK'
+keywordfile = 'keywords.txt'
+keywords = []
+with open(keywordfile, 'r') as f:
+    keywords = f.read().split()
+
+ac_list = []
+query_words = re.split('(\W+)', query)
+print(query_words)
+print(keywords)
+
+ac_list = [word for word in query_words if word in keywords]
+print(ac_list)
+
+triplets = []
+for i in range(len(ac_list)-1):
+    q_block = re.search(''+ac_list[i]+' *.* '+ac_list[i+1]+'', query)
+    triplets.append([ac_list[i],ac_list[i+1],q_block.group(0)])
+
+print(triplets)
